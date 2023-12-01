632ee1f67eb627e56ea345d8733076ad4dec1550
Jonathan Jensen <jonathan.obsidion@gmail.com>
2023-12-01 21:53:02 +0100

    Refactored `DepartureTest` to use the `@BeforeEach` decorator

--- src/test/java/edu/ntnu/stud/DepartureTest.java
+++ src/test/java/edu/ntnu/stud/DepartureTest.java
@@ -1,12 +1,15 @@
 package edu.ntnu.stud;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.Optional;
 
 import java.time.LocalTime;
 
 public class DepartureTest {
+  private Departure dep;
   @Test
   void constructor() {
     assertThrows(IllegalArgumentException.class, () -> new Departure(LocalTime.of(0, 0), "", "Trondheim"));
@@ -15,9 +18,13 @@ public class DepartureTest {
     assertDoesNotThrow(() -> new Departure(LocalTime.of(0, 0), "F1", "Trondheim"));
   }
 
+  @BeforeEach
+  void initTestData() {
+    dep = new Departure(LocalTime.of(12, 30), "A4", "Oslo S");
+  }
+
   @Test
   void setTrack() {
-    var dep = new Departure(LocalTime.of(12, 30), "F1", "Trondheim");
     assertDoesNotThrow(() -> dep.setTrack(3));
     assertThrows(IllegalArgumentException.class, () -> dep.setTrack(-3));
     assertThrows(IllegalArgumentException.class, () -> dep.setTrack(0));
@@ -25,13 +32,11 @@ public class DepartureTest {
 
   @Test
   void getTime() {
-    var dep = new Departure(LocalTime.of(12, 30), "F1", "Trondheim");
     assertEquals(LocalTime.of(12, 30), dep.getTime());
   }
 
   @Test
   void getRealTime() {
-    var dep = new Departure(LocalTime.of(12, 30), "F1", "Trondheim");
     assertEquals(LocalTime.of(12, 30), dep.getRealTime());
     dep.setDelay(LocalTime.of(0, 30));
     assertEquals(LocalTime.of(13, 0), dep.getRealTime());
@@ -39,19 +44,16 @@ public class DepartureTest {
 
   @Test
   void getLine() {
-    var dep = new Departure(LocalTime.of(12, 30), "F1", "Trondheim");
-    assertEquals("F1", dep.getLine());
+    assertEquals("A4", dep.getLine());
   }
 
   @Test
   void getDestination() {
-    var dep = new Departure(LocalTime.of(12, 30), "F1", "Trondheim");
-    assertEquals("Trondheim", dep.getDestination());
+    assertEquals("Oslo S", dep.getDestination());
   }
 
   @Test
   void getTrack() {
-    var dep = new Departure(LocalTime.of(12, 30), "F1", "Trondheim");
     assertEquals(Optional.empty(), dep.getTrack());
     dep.setTrack(3);
     assertEquals(Optional.of(3), dep.getTrack());
@@ -59,7 +61,6 @@ public class DepartureTest {
 
   @Test
   void getDelay() {
-    var dep = new Departure(LocalTime.of(12, 30), "F1", "Trondheim");
     assertEquals(LocalTime.of(0, 0), dep.getDelay());
     dep.setDelay(LocalTime.of(0, 30));
     assertEquals(LocalTime.of(0, 30), dep.getDelay());
@@ -67,7 +68,6 @@ public class DepartureTest {
 
   @Test
   void setDelay() {
-    var dep = new Departure(LocalTime.of(12, 30), "F1", "Trondheim");
     dep.setDelay(LocalTime.of(0, 30));
     assertEquals(LocalTime.of(0, 30), dep.getDelay());
   }
